0c6e234b6f707e07a9b13e8521d5e4894790883c
cwilkins-8 <71436174+cwilkins-8@users.noreply.github.com>
2020-11-30 23:05:34 +0000

    Add files via upload

---
+++ 7.8.py
@@ -0,0 +1,15 @@
+#7.8
+#Deli: Make a list called sandwich_orders and fill it with the names of various sandwiches. Then make an empty list called finished_sandwiches. Loop
+#through the list of sandwich orders and print a message for each order, such
+#as I made your tuna sandwich. As each sandwich is made, move it to the list
+#of finished sandwiches. After all the sandwiches have been made, print a
+#message listing each sandwich that was made.
+
+sandwich_orders = ['ham', 'cheese', 'tuna']
+finished_orders = []
+
+while sandwich_orders:
+    finished_order = sandwich_orders.pop()
+    
+    print("I made your " + sandwich_orders.title() + "sandwich!")
+    finished_orders.append(sandwich_orders)
