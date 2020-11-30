75e5a85b28f69e9866390a72df1cabb07d0c5f12
cwilkins-8 <71436174+cwilkins-8@users.noreply.github.com>
2020-11-30 23:05:03 +0000

    Add files via upload

---
+++ 7.2.py
@@ -0,0 +1,21 @@
+#7.2 Restaurant Seating
+
+#seating = input("How many people are in your dinner group? ")
+#seating = int(seating)
+
+#if seating <= 8:
+    #print("\n We have a table avaible for you")
+#else:
+   # print("Sorry, you will have to wait for a table.")
+
+
+
+#7.3 Multiples of Ten 
+
+number = input("Please enter a number : ")
+number =int(number)
+
+if number % 10 == 0 :
+    print("\n This number is a multiple of 10!")
+else:
+    print("This number is not a multiple of 10")
